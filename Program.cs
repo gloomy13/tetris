25766d1af33c419d2a94bcb78f1b6601792dceb1
gloomy13 <74317881+gloomy13@users.noreply.github.com>
2021-07-02 16:32:59 +0200

    Main file

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gra gra=new Gra(10,15);
+            gra.Play();
+            
+
+            Console.ReadLine();
+        }
+    }
+}
